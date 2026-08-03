b1d125509c45c1e24747d532527836feb6baced6
zack <108603099+DrXander@users.noreply.github.com>
2026-08-02 20:42:58 -0500

    playermovement + laser

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float speed = 0.5f;
+    public float moveSpeed = 0.5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    public GameObject laserPrefab;
+    public Transform firePoint;
+    public float timeBetweenShots = 0.5f;
+    private float shotTimer;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,10 +26,28 @@ public class PlayerMovement : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput.Normalize();
+        
+
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        Vector2 mouseDistance = mousePos - screenPoint;
+        float angle = Mathf.Atan2(mouseDistance.y, mouseDistance.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        if (Input.GetMouseButton(0) && shotTimer <= 0f)
+        {
+            Instantiate(laserPrefab, firePoint.position, firePoint.rotation);
+            shotTimer = timeBetweenShots;
+        }
+
+        if (shotTimer > 0f)
+        {
+            shotTimer -= Time.deltaTime;
+        }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
     }
 }
